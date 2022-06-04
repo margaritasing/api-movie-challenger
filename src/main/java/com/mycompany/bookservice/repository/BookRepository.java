83b965bb4daf3eb8ff1e47a043b3dc6bbfbe38c4
margaritasing <libiamfreitesm@gmail.com>
2022-06-04 17:35:44 -0300

    subo repo JPA

---
+++ src/main/java/com/mycompany/bookservice/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.mycompany.bookservice.repository;
+
+import com.mycompany.bookservice.entity.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
+}
