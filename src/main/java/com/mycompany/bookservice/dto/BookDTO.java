05365dbf2597d8b0c4231531143a067385b19a72
margaritasing <libiamfreitesm@gmail.com>
2022-06-04 17:35:17 -0300

    subo dto

---
+++ src/main/java/com/mycompany/bookservice/dto/BookDTO.java
@@ -0,0 +1,18 @@
+package com.mycompany.bookservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookDTO {
+
+    private Long bookId;
+    private String name;
+    private String description;
+    private Double pricePerQty;
+    private Double availableQty;
+    private String authorName;
+    private String authorEmail;
+
+}
