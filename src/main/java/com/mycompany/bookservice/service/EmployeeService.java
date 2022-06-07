47e8c11cc4eed9d21c24503ac74f4776e021b6eb
margaritasing <libiamfreitesm@gmail.com>
2022-06-07 12:10:05 -0300

    subo employee service

--- src/main/java/com/mycompany/bookservice/service/EmployeeService.java
+++ src/main/java/com/mycompany/bookservice/service/EmployeeService.java
@@ -1,4 +1,14 @@
 package com.mycompany.bookservice.service;
 
+import com.mycompany.bookservice.dto.EmployeeDTO;
+
+import java.util.List;
+
 public interface EmployeeService {
+    public EmployeeDTO addEmployeee(EmployeeDTO employeeDTO);
+    public EmployeeDTO updateEmployee(EmployeeDTO employeeDTO, Long empId);
+    public void deleteEmployee(Long bookId);
+    public List<EmployeeDTO> getAllEmployee();
+    public EmployeeDTO getEmployee(Long empId);
+
 }
