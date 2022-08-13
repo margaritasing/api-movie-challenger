b048adf1a0951e66a4a1a074d98ad0945d84b377
margaritasing <libiamfreitesm@gmail.com>
2022-08-13 19:30:52 -0300

    agrego dependencias y elimino algunos archivos

--- src/main/java/com/mycompany/bookservice/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.bookservice.repository;
-
-import com.mycompany.bookservice.entity.BookEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<BookEntity, Long> {
-}
